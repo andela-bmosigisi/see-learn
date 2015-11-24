d96a63a1ed4e665e2b5bc27f8f8daaf16be5c246
andela.bmosigisi <brianmosigisi@yahoo.com>
2015-11-24 15:52:40 +0300

    add master layout

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,57 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/favicon.ico">
+@extends('layouts.master')
 
-    <title>See-learn</title>
+@section('content')
+  @include('partials.navigation')
+  <!-- Jumbotron -->
+  <div class="jumbotron">
+    <h1>Marketing stuff!</h1>
+    <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
+    <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+  </div>
 
-    <!-- Bootstrap core CSS -->
-    <link href="/css/app.css" rel="stylesheet">
-  </head>
+  <!-- Example row of columns -->
+  <div class="row">
+    <div class="col-lg-4">
+      <h2>Heading</h2>
+      <p>Put video here.</p>
+      <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+    </div>
+    <div class="col-lg-4">
+      <h2>Heading</h2>
+      <p>Put video here.</p>
+      <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+   </div>
+    <div class="col-lg-4">
+      <h2>Heading</h2>
+      <p>Put video here.</p>
+      <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+    </div>
+  </div>
+  <!-- Site footer -->
+  <footer class="footer">
+    <p>&copy; Company 2014</p>
+  </footer>
+@endsection
 
-  <body>
-
-    <div class="container">
-
-      @include('partials.navigation')
-      <!-- Jumbotron -->
-      <div class="jumbotron">
-        <h1>Marketing stuff!</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
-      </div>
-
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Put video here.</p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Put video here.</p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Put video here.</p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
-
-      <!-- Site footer -->
-      <footer class="footer">
-        <p>&copy; Company 2014</p>
-      </footer>
-
-    </div> <!-- /container -->
-
-  </body>
-</html>
