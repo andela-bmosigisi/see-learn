2ad6d7e134b881b54d213e1c33018b2b0cad6e6b
andela.bmosigisi <brianmosigisi@yahoo.com>
2015-11-24 15:44:47 +0300

    add navigation partial

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -19,17 +19,7 @@
 
     <div class="container">
 
-      <!-- The justified navigation menu is meant for single line per list item.
-           Multiple lines will require custom code not provided by Bootstrap. -->
-      <div class="masthead">
-        <h3 class="text-muted">See, learn.</h3>
-        <nav>
-          <ul class="nav nav-justified">
-            <li class="active"><a href="#">Home</a></li>
-          </ul>
-        </nav>
-      </div>
-
+      @include('partials.navigation')
       <!-- Jumbotron -->
       <div class="jumbotron">
         <h1>Marketing stuff!</h1>
---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,8 @@
+<div class="masthead">
+  <h3 class="text-muted">See, learn.</h3>
+  <nav>
+    <ul class="nav nav-justified">
+      <li class="active"><a href="#">Home</a></li>
+    </ul>
+  </nav>
+</div>
