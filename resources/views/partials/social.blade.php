cc37de6dc85405e22164dafd7c72032e7161f387
andela.bmosigisi <brianmosigisi@yahoo.com>
2015-12-04 12:25:51 +0300

    add basic style scaffolding

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,8 +1,12 @@
 <div class="social">
   <!-- Social login buttons here -->
-  <ul>
-    <li><a href="/login/facebook"><img src="/img/fb.png"></a></li>
-    <li><a href="/login/github"><img src="/img/github.png"></a></li>
-    <li><a href="/login/twitter"><img src="/img/twitter.png"></a></li>
-  </ul>
+    <a class="btn btn-block btn-social btn-facebook" href="/login/facebook">
+        <span class="fa fa-facebook"></span> Sign in with Facebook
+    </a>
+    <a class="btn btn-block btn-social btn-twitter" href="login/twitter">
+        <span class="fa fa-twitter"></span> Sign in with Twitter
+    </a>
+    <a class="btn btn-block btn-social btn-github" href="login/github">
+        <span class="fa fa-github"></span> Sign in with Github
+    </a>
 </div>
