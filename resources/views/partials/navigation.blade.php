5ffc00df06c3bf863898c982aa842c2de7bcf7cf
andela.bmosigisi <brianmosigisi@yahoo.com>
2015-12-05 22:09:51 +0300

    refine views

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,24 +1,30 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
+    <div class="navbar-header">   
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">See, learn.</a>
+      <a class="navbar-brand" href="/">See, learn.</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+      <form class="navbar-form navbar-left nav-search" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" id="nav-search" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Search</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+      @if (isset($user))
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            {{ $user->name }}
+            <span class="caret"></span>
+          </a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
+            <li><a href="/user/{{ $user->id }}/edit">Profile</a></li>
+            <li><a href="/logout">Sign Out</a></li>
             <li><a href="#">Something else here</a></li>
             <li class="divider"></li>
             <li><a href="#">Separated link</a></li>
@@ -26,15 +32,10 @@
             <li><a href="#">One more separated link</a></li>
           </ul>
         </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Search</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
+      @else
+        <li><a href="/login">Login</a></li>
+        <li><a href="/register">Register  </a></li>
+      @endif
       </ul>
     </div>
   </div>
